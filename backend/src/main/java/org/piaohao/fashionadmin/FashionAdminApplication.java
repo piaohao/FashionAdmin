a887c3baf314473ce065a8286ef096dd046aeaea
piaohao <463168428@qq.com>
2018-11-27 18:51:11 +0800

    init

---
+++ backend/src/main/java/org/piaohao/fashionadmin/FashionAdminApplication.java
@@ -0,0 +1,12 @@
+package org.piaohao.fashionadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionAdminApplication.class, args);
+    }
+}
